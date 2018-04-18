ecc5488ced11b4f91b381874af385fb6f669fcab
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-18 11:46:06 +0200

    Zahlenreihe mit geraden Zahlen

--- zahlenreihe.py
+++ zahlenreihe.py
@@ -6,3 +6,14 @@ for i in range(1,n+1):
         print(i, end=" < ")
     else:
         print(i)
+
+n
+for i in range(1,n+1):
+    # alle ungeraden Zahlen von 1 bis n ausgeben
+    if (i % 2):
+        print (i, "ist ungerade")
+    # alle geraden Zahlen von 1 bis n ausgebe
+    
+        
+    
+# alle Zahlen von 1 bis n ausgeben, die durch 9 teilbar sinds
