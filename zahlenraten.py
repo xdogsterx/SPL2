52a72d56335d53f0aef4194b67d1d27285302df2
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-18 12:23:25 +0200

    Version 1

--- zahlenraten.py
+++ zahlenraten.py
@@ -1,1 +1,14 @@
 #zahlenraten.py
+
+import random
+zufallszahl = random.randint(1,100)
+# Benutzer soll die Zahl vom Computer erraten
+while (True):
+    versuch = input("Geben Sie ihre schätzung ein: ")
+    if(versuch == zufallszahl):
+        print("Gratulation")
+        break
+    else:
+        print("Versuchen Sie es erneut")
+    
+# Ausgabe: wie viele Versuche waren nötig?
