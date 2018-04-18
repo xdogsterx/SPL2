2b82a3c3ea687842636a5c2c49d7f37a91c9b583
xdogsterx <171witb16@o365.htl-leoben.at>
2018-04-18 12:04:32 +0200

    Zahlenreihe die die ungeraden, geraden und durch 9 teilbaren Zahlen ausgibt

--- zahlenreihe.py
+++ zahlenreihe.py
@@ -20,5 +20,12 @@ for i in range(1,n+1):
     if (i % 2 == 0):
         print (i, end=" ")
     
-    
+print("")
 # alle Zahlen von 1 bis n ausgeben, die durch 9 teilbar sind
+for i in range(1,n+1):
+    if(i%9 == 0):
+        if(i+1 !=n):
+            print(i, end=", ")
+        else:
+            print(i)
+        
